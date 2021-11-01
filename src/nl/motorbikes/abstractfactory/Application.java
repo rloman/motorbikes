6aab546acb7c4dd77b47e3668b2776dbcfe46e57
Raymond Loman <raymond@carpago.nl>
2021-11-01 13:04:06 +0100

    Finish main and replace return null to unsup.oper.exception

--- src/nl/motorbikes/abstractfactory/Application.java
+++ src/nl/motorbikes/abstractfactory/Application.java
@@ -1,14 +1,13 @@
 package nl.motorbikes.abstractfactory;
 
-import nl.motorbikes.model.Motor;
 import nl.motorbikes.model.Vehicle;
 import nl.motorbikes.model.VehicleFactory;
 
 public class Application {
 
     public static void main(String[] args) {
+
         Vehicle m = VehicleFactory.getVehicle("motor");
-
-
+        System.out.println(m.getClass().getName());
     }
 }
