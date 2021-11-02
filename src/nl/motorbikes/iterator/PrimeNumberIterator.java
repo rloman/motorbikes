e4155f7c65cb189ea503df7f36457f94ec129df8
Raymond Loman <raymond@carpago.nl>
2021-11-02 15:42:17 +0100

    Work in progress ...

---
+++ src/nl/motorbikes/iterator/PrimeNumberIterator.java
@@ -0,0 +1,18 @@
+package nl.motorbikes.iterator;
+
+import java.util.Iterator;
+
+public class PrimeNumberIterator implements Iterator<Integer> {
+
+
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        return null;
+    }
+}
