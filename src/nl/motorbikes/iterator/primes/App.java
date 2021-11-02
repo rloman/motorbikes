416ad36a6c1bad487e5bd08578fa8e1ad61e213a
Raymond Loman <raymond@carpago.nl>
2021-11-02 21:46:27 +0100

    Add /finish prime iterator

--- src/nl/motorbikes/iterator/primes/App.java
+++ src/nl/motorbikes/iterator/primes/App.java
@@ -4,5 +4,10 @@ public class App {
 
     public static void main(String[] args) {
 
+        PrimeNumberIterator i = new PrimeNumberIterator(100);
+        for (PrimeNumberIterator it = i; it.hasNext(); ) {
+            int element = it.next();
+            System.out.println(element);
+        }
     }
 }
--- src/nl/motorbikes/iterator/primes/PrimeNumberIterator.java
+++ src/nl/motorbikes/iterator/primes/PrimeNumberIterator.java
@@ -4,25 +4,86 @@ import java.util.Iterator;
 
 public class PrimeNumberIterator implements Iterator<Integer> {
 
-    private int prime = 2;
-    private int max = 2;
+    /**
+     * The next prime to be returned by this iterator's next method.
+     */
+    private int nextPrime;
+    private int maxPrime = 2;
 
+    /**
+     * Creates an iterator over all of the prime numbers.
+     */
 
     public PrimeNumberIterator(int max) {
-        this.max = max;
+        nextPrime = 2;
+        this.maxPrime = max;
     }
 
-    @Override
-    public boolean hasNext() {
-        return prime < max;
+    /**
+     * Sets <CODE>nextPrime</CODE> to the smallest prime number less than
+     * the given integer.
+     *
+     * @param n an integer
+     */
+
+    private void setNextPrime(int n) {
+        if (n < 2) {
+            nextPrime = 2;
+        } else {
+            int candidate = n + 1;
+
+            // ensure candidate is odd
+
+            if (candidate % 2 == 0)
+                candidate++;
+
+            while (!isPrime(candidate))
+                candidate += 2;
+
+            nextPrime = candidate;
+        }
     }
 
-    @Override
+    /**
+     * Determines if the given odd integer is prime.
+     *
+     * @param n an odd integer strictly greater than 1
+     */
+
+    private boolean isPrime(int n) {
+        // search for odd factors between 3 and sqrt(n)
+
+        int factor = 3;
+
+        while (factor * factor <= n && n % factor > 0)
+            factor += 2;
+
+        return (factor * factor > n);
+    }
+
+    /**
+     * Returns the next prime number in this iteration.
+     *
+     * @return the next prime number in this iteration
+     */
+
     public Integer next() {
-        return null;
+        int prime = nextPrime;
+
+        setNextPrime(nextPrime);
+
+        return prime;
     }
 
-    private int getNextPrime() {
+    /**
+     * Determines if there is a next prime in this iteration.
+     * This implementation returns true even though there is a bound on the
+     * value of an <CODE>Integer</CODE>.
+     *
+     * @return true
+     */
 
+    public boolean hasNext() {
+        return this.nextPrime <= this.maxPrime;
     }
 }
